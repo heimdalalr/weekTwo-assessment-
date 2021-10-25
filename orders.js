118623dbbf29a009a28dba97e021a87d7f69d9bc
heimdalalr <lamorliere@gmail.com>
2021-10-24 23:45:10 -0400

    orders js problems completed

--- orders.js
+++ orders.js
@@ -29,7 +29,18 @@
     stored on the object.
 */
 
-//CODE HERE
+class ticket {
+    status = "queued"
+    constructor(items, orderTime, customerID) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerID = customerID;
+    }
+    updateStatus(newStatus){
+        this.status = newStatus;
+        console.log("The order for customer "+this.customerID+" is now "+this.status+".");
+    }
+}
 
 
 
@@ -44,7 +55,7 @@
     customer: 575
 */
 
-//CODE HERE
+let firstTicket = new Ticket(["pizza", "bread", "soda"], "7:03 PM", "575");
 
 
 /*
@@ -53,4 +64,4 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus("cooking");
